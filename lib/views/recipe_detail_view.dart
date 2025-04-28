1eb35fa5ea2fe543355a095c63e1f0001ec354e3
rabab_hamdyy <rababhamdy38@gmail.com>
2025-04-28 07:59:56 +0300

    wave clipper

--- lib/views/recipe_detail_view.dart
+++ lib/views/recipe_detail_view.dart
@@ -8,32 +8,64 @@ class RecipeDetailView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(300),
-        child: AppBar(
-          foregroundColor: Colors.white,
-          elevation: 0,
-          leading: Container(
-            margin: EdgeInsets.only(left: 10),
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              color: Colors.lime,
-            ),
-            child: IconButton(
-              icon: Icon(Icons.arrow_back),
-              onPressed: () {
-                Navigator.pop(context);
-              },
-            ),
+      body: Column(
+        children: [
+          Stack(
+            children: [
+              ClipPath(
+                clipper: WaveClipper(),
+                child: Image.network(
+                  recipe.imageUrl,
+                  fit: BoxFit.cover,
+                  width: double.infinity,
+                  height: 400,
+                ),
+              ),
+              Positioned(
+                top: 30,
+                left: 20,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.lime,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: IconButton(
+                    icon: Icon(Icons.arrow_back, color: Colors.white, size: 25),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                ),
+              ),
+            ],
           ),
-          flexibleSpace: Image.network(
-            recipe.imageUrl,
-            fit: BoxFit.cover,
-            width: double.infinity,
+          Expanded(
+            child: Container(color: Colors.white, padding: EdgeInsets.all(20)),
           ),
-        ),
+        ],
       ),
-      body: Center(child: Text("Recipe Details will be shown here.")),
     );
   }
+}
+
+class WaveClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    var path = Path();
+    path.lineTo(0, size.height * 0.8);
+
+    path.cubicTo(
+      size.width * 0.2,
+      size.height,
+      size.width * 0.5,
+      size.height * 0.6,
+      size.width,
+      size.height * 0.8,
+    );
+    path.lineTo(size.width, 0);
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
 }
